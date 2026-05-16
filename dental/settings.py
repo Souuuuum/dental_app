05efde2afb6d65df6d47e25492342f2857b6325c
soum <soum@gamil.com>
2026-05-16 13:19:35 +0100

    ready deploy

--- dental/settings.py
+++ dental/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'dental.wsgi.application'
+
 
 
 # Database
@@ -117,4 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR ,'staticfiles')
 LOGIN_URL = '/login/'
